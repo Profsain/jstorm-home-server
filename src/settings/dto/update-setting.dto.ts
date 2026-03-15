577812ecee01a7db67e3abfd1fd38037b29b15c7
Profsain <husseinimudiking@gmail.com>
2026-03-15 05:42:39 +0100

    fix

--- src/settings/dto/update-setting.dto.ts
+++ src/settings/dto/update-setting.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateSettingDto {
   @ApiProperty({ example: '+2348000000000' })
   @IsString()
+  @IsOptional()
   whatsappNumber?: string;
 
   @ApiProperty({ example: '+44737015649' })
